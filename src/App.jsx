92b6d9537ddf246e846a0664aac146e93d61ea71
Maliki <abdoulmalikntwari@gmail.com>
2024-07-20 00:42:54 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ProductPage from "./components/ProductPage"
+
+function App() {
+  
+
+  return (
+   <div className="max-w-7xl  mx-auto">
+    <ProductPage />
+   </div> 
+  )
+}
+
+export default App
